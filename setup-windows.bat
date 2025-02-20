b8c9f0c97571735a49916afcf6f25336b4f300fc
Lilien <lilienauger.86@gmail.com>
2025-02-20 08:26:28 +0100

    fix: remove mac, and add good path

--- setup-windows.bat
+++ setup-windows.bat
@@ -9,5 +9,5 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 echo Setup complete! Run the app using:
-echo venv\Scripts\activate && python app.py
+echo venv\Scripts\activate && python src\app.py
 pause
